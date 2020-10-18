e7f2a3e3b7c43d74c1eb927deb16cc57ffe07ab2
maxotto <agmfromita@mail.ru>
2020-10-18 17:05:46 +0300

    delete sample project

--- delivery_app_bg/lib/background_main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_background/locationService.dart';
-
-void backgroundMain() {
-  WidgetsFlutterBinding.ensureInitialized();
-  LocationService();
-}
--- delivery_app_bg/lib/constants.dart
+++
@@ -1,5 +0,0 @@
-// final String serverUri = "ws://mikhailichenko.ru";
-final String serverUri =
-    "ws://ec2-18-218-34-205.us-east-2.compute.amazonaws.com";
-final int port = 9001;
-final String topicName = "delivery/andrey/location";
